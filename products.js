030ceea55e9f6949e391e9e35dd57b1fc1548729
Kabir <kabirajibade@yahoo.com>
2023-02-05 21:54:00 +0100

    unfinished

--- products.js
+++ products.js
@@ -1,7 +1,8 @@
-fetch('https://dummyjson.com/products')
+// if(document.getElementById('find').innerHTML=""){
+   fetch('https://dummyjson.com/products')
 .then(res => res.json())
 .then(res =>{
-    console.log(res.products);
+   //  console.log(res.products);
 let theProducts=res.products;
 
  // .then(console.log);
@@ -15,8 +16,15 @@ let theProducts=res.products;
     let brand=res.products[i].brand;
     let cartegory=res.products[i].category;
     let thumbnail=res.products[i].thumbnail;
+   //  let images=res.products[i].images
+   //  for (k=0; k<5;k++){
+   //    let pictures=document.createElement('img');
+   //    pictures.src=images;
+   //    pictures.setAttribute('class','pictures');
+   //    console.log(pictures)
+      
+   //  }
 
-    // document.querySelector('')
     let mainDiv=document.getElementById('items')
  let pTag= document.createElement('p');
  let pTag1= document.createElement('p');
@@ -36,8 +44,7 @@ let theProducts=res.products;
  pTag7.innerHTML="Brand: "+cartegory;
  let div1=document.createElement('div');
  let img=document.createElement('img')
-//  let div2=document.getElementById('two');
-//  let div3=document.getElementById('three')
+
 img.setAttribute('class', 'images')
 div1.setAttribute('class','one col-12 col-md-4 col-sm-12')
  pTag.setAttribute('class','id')
@@ -59,12 +66,95 @@ img.src=thumbnail;
  div1.append(pTag6)
  div1.append(pTag7)
  div1.append(img)
+
  mainDiv.appendChild(div1)
  img.setAttribute('alt','Phones')
 
-    console.log(prodId);
-    console.log(prodTitle);
-    console.log(prodPrice);
+   //  console.log(prodId);
+   //  console.log(prodTitle);
+   //  console.log(prodPrice);
  }
 })
-             
+
+// }
+// else{}
+ function search(){
+   let Title=document.getElementById('find').value;
+       fetch(`https://dummyjson.com/products/search?q=${Title}`)
+       .then(res => res.json())
+         //  .then(console.log);
+      .then(res=>{
+         // let theProducts=[]
+        let   theProducts=res.products;
+            // console.log(res.products.title)
+            let mainDiv=document.getElementById('items')
+            console.log(mainDiv.childElementCount)
+         for(let i=0;i<theProducts.length; i++){
+            let prodId=res.products[i].id;
+            let prodTitle=res.products[i].title;
+            let prodPrice=res.products[i].price;
+            let description=res.products[i].description;
+            let discount=res.products[i].discountPercentage;
+            let rating=res.products[i].rating;
+            let brand=res.products[i].brand;
+            let cartegory=res.products[i].category;
+            let thumbnail=res.products[i].thumbnail;
+            // console.log(prodTitle)
+        
+         // let mainDiv=document.getElementById('items')
+         let pTag= document.createElement('p');
+         let pTag1= document.createElement('p');
+         let pTag2= document.createElement('p');
+         let pTag3= document.createElement('p');
+         let pTag4= document.createElement('p');
+         let pTag5= document.createElement('p');
+         let pTag6= document.createElement('p');
+         let pTag7= document.createElement('p');
+         let testP=document.createElement('p');
+         pTag.innerHTML="Product number "+prodId;
+         pTag1.innerHTML="Product Name "+prodTitle;
+         pTag2.innerHTML="Price  $"+prodPrice;
+         pTag3.innerHTML="Description: "+description;
+         pTag4.innerHTML="Discount "+discount+"%"
+         pTag5.innerHTML="Rating: "+rating;
+         pTag6.innerHTML=brand;
+         pTag7.innerHTML="Brand: "+cartegory;
+         let div1=document.createElement('div');
+         let img=document.createElement('img')
+        img.setAttribute('class', 'images')
+        div1.setAttribute('class','one col-12 col-md-4 col-sm-12')
+         pTag.setAttribute('class','id')
+         pTag1.setAttribute('class','title')
+         pTag2.setAttribute('class','price')
+         pTag3.setAttribute('class','description')
+         pTag4.setAttribute('class','discount')
+         pTag5.setAttribute('class','rating')
+         pTag6.setAttribute('class','brand')
+         pTag7.setAttribute('class','cartegory')
+         testP.setAttribute('class','id')
+         mainDiv.setAttribute('class','row text-bg-light mx-3 container-fluid')
+         div1.appendChild(pTag)
+        img.src=thumbnail;
+      //   console.log(prodTitle)
+         div1.append(pTag1)
+         div1.append(pTag2)
+         div1.append(pTag3)
+         div1.append(pTag4)
+         div1.append(pTag5)
+         div1.append(pTag6)
+         div1.append(pTag7)
+         div1.append(img)
+         mainDiv.appendChild(div1)
+         img.setAttribute('alt','Phones')
+         // console.log(prodId);
+         console.log(prodTitle);
+         // console.log(prodPrice);
+        
+        
+         }
+       
+        
+                       
+            })
+         
+             }
